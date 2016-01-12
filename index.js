70c7b593c07ba23aa0d867348d12c9c45539dcee
tianxiang.wly <tianxiang.wly@alibaba-inc.com>
2016-01-13 00:55:20 +0800

    :smile:

--- index.js
+++ index.js
@@ -3,18 +3,35 @@ const program = require('commander');
 const path = require('path');
 const pkg = require(path.join(__dirname, 'package.json'));
 
+const publishers = {
+  git: require('./lib/git'),
+  node: require('./lib/node')
+};
+
+const DEFAULT_CONFIG = {
+  checkCommitted: 2,
+  checkTracked: 2,
+  checkBranch: 2,
+  checkTaggable: 2,
+  checkDepVersion: 2,
+  checkChangelog: 2,
+  checkVersion: 2,
+  runPrepublish: true,
+  runTest: true,
+  tagFormat: `<%= version %>`,
+  branchFormat: `<%= version %>`
+};
 
 program.version(pkg.version)
-  .usage('[options] <type>')
-  .option('-t, --test', 'Run test')
-  .action((type, prog) => {
-    if (type === 'git') {
-      publishGit();
-    } else if (type === 'node') {
-      publishNode();
-    } else {
+  .usage('[options] <version>')
+  .option('-c, --cwd', 'Project path')
+  .option('-t, --type [type]', 'Use default publish type')
+  .action((version, prog) => {
+    const cwd = prog.cwd || process.cwd();
+    if (type && publishers[type])
+      publishers[type](version, cwd, DEFAULT_CONFIG);
+    else
       program.help();
-    }
   });
 
 module.exports = program;
